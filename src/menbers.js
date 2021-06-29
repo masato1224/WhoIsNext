54868589d4dc39e4df5aa34292530d1e02753e04
masato1224 <masatomasato1224@gmail.com>
2021-06-29 10:37:05 +0900

    メンバー修正 メンバー修正

--- src/menbers.js
+++ src/menbers.js
@@ -2,15 +2,17 @@
 export const list = [
   { name: "金子", color: "#909" },
   { name: "たけさん", color: "#906" },
-  { name: "佐藤", color: "#903" },
-  { name: "渡辺", color: "#900" },
-  { name: "南", color: "#609" },
+  { name: "ゆっき〜☆", color: "#903" },
+  { name: "なべちゃん", color: "#900" },
+  { name: "South", color: "#609" },
   { name: "鈴木", color: "#939" },
+  { name: "どらごん", color: "#900" },
 
-  { name: "竹内", color: "#936" },
-  { name: "山田", color: "#933" },
+  { name: "横浜流星", color: "#936" },
+  { name: "まさすぃ〜☆", color: "#933" },
   { name: "なびちゃん", color: "#930" },
 
+  { name: "みはるん", color: "#200" },
   { name: "鶴飼", color: "#309" },
   { name: "いのす", color: "#639" },
   { name: "濱ちゃん", color: "#969" }
